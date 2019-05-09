8cd4de02aa5aa4dc0540553e5a19c1dc51725430
Eric Riebling <er1k@andrew.cmu.edu>
2019-05-09 16:39:53 -0400

    update

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,152 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HoloToolkit.Unity
+{
+    public enum PivotAxis
+    {
+        // Most common options, preserving current functionality with the same enum order.
+        XY,
+        Y,
+        // Rotate about an individual axis.
+        X,
+        Z,
+        // Rotate about a pair of axes.
+        XZ,
+        YZ,
+        // Rotate about all axes.
+        Free
+    }
+
+    /// <summary>
+    /// The Billboard class implements the behaviors needed to keep a GameObject oriented towards the user.
+    /// </summary>
+    public class Billboard : MonoBehaviour
+    {
+        private Vector3 latePosition;
+        private Quaternion lateRotation;
+
+        /// <summary>
+        /// The axis about which the object will rotate.
+        /// </summary>
+        [Tooltip("Specifies the axis about which the object will rotate.")]
+        [SerializeField]
+        private PivotAxis pivotAxis = PivotAxis.Y;
+        public PivotAxis PivotAxis
+        {
+            get { return pivotAxis; }
+            set { pivotAxis = value; }
+        }
+
+        /// <summary>
+        /// The target we will orient to. If no target is specified, the main camera will be used.
+        /// </summary>
+        [Tooltip("Specifies the target we will orient to. If no target is specified, the main camera will be used.")]
+        private Transform cameraTransform;
+        public Transform CameraTransform
+        {
+            get { return cameraTransform; }
+            set { cameraTransform = value; }
+        }
+
+        private void OnEnable()
+        {
+            if (CameraTransform == null)
+            {
+                CameraTransform = Camera.main.transform;
+            }
+
+            Update();
+        }
+
+        private void Start()
+        {
+            
+        }
+        private void LateUpdate()
+        {
+            transform.position = latePosition;
+            transform.rotation = lateRotation;
+        }
+
+        /// <summary>
+        /// Keeps the object facing the camera.
+        /// </summary>
+        private void Update()
+        {
+            if (CameraTransform == null)
+            {
+                return;
+            }
+
+            // Get a Vector that points from the target to the main camera.
+            Vector3 directionToTarget = CameraTransform.position - transform.position;
+            Vector3 targetUpVector = Camera.main.transform.up;
+
+            // Get location 1m in front of camera
+
+            Matrix4x4 m = Camera.main.cameraToWorldMatrix;
+            Vector3 p = m.MultiplyPoint(new Vector3(0, 0, -1.0F));
+            latePosition = p;
+
+//            transform.position = new Vector3(CameraTransform.position.x + 0.5f,
+//                CameraTransform.position.y - 0.5f,
+//                CameraTransform.position.z + 1);
+
+//            Text myText = GameObject.Find("Canvas").GetComponent<Text>();
+
+            // Adjust for the pivot axis.
+            switch (PivotAxis)
+            {
+                case PivotAxis.X:
+                    directionToTarget.x = 0.0f;
+                    targetUpVector = Vector3.up;
+                    break;
+
+                case PivotAxis.Y:
+                    directionToTarget.y = 0.0f;
+                    targetUpVector = Vector3.up;
+                    break;
+
+                case PivotAxis.Z:
+                    directionToTarget.x = 0.0f;
+                    directionToTarget.y = 0.0f;
+                    break;
+
+                case PivotAxis.XY:
+                    targetUpVector = Vector3.up;
+                    break;
+
+                case PivotAxis.XZ:
+                    directionToTarget.x = 0.0f;
+                    break;
+
+                case PivotAxis.YZ:
+                    directionToTarget.y = 0.0f;
+                    break;
+
+                case PivotAxis.Free:
+                default:
+                    // No changes needed.
+                    break;
+            }
+
+            // If we are right next to the camera the rotation is undefined. 
+            if (directionToTarget.sqrMagnitude < 0.001f)
+            {
+                return;
+            }
+
+//            myText.text = directionToTarget.x + Environment.NewLine +
+//                directionToTarget.y + Environment.NewLine +
+//                directionToTarget.z;
+
+            // Calculate and apply the rotation required to reorient the object
+            lateRotation = Quaternion.LookRotation(-directionToTarget, targetUpVector);
+        }
+    }
+}
---
+++ Assets/Scripts/SphereCommands2.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class SphereCommands2 : MonoBehaviour
+{
+    private const int listenPort = 9006;
+    UdpClient listener;
+
+    string received_data;
+    Vector3 origin = new Vector3(0, 0, 0);
+    Transform tranny;
+    Text myText;
+    ThirdPersonCharacter c;
+    Animator anim;
+
+    void Start()
+	{
+        System.Diagnostics.Debug.WriteLine("Starting...");
+
+        IPEndPoint ep = new IPEndPoint(IPAddress.Any, listenPort);
+      
+        listener = new UdpClient(ep);
+        System.Diagnostics.Debug.WriteLine(listener.ToString());
+        System.Diagnostics.Debug.WriteLine(listener.Client.ToString());
+
+        string hostname = System.Net.Dns.GetHostName();
+        System.Diagnostics.Debug.WriteLine(hostname);
+        string myIP = System.Net.Dns.GetHostEntry(hostname).AddressList[0].ToString();
+        System.Diagnostics.Debug.WriteLine("IP is: "+myIP);
+
+        try {
+            IAsyncResult iar = listener.BeginReceive(new AsyncCallback(recv), null);
+            System.Diagnostics.Debug.WriteLine("Done initializing, listener" + iar.ToString());
+        }
+        catch(Exception e) {
+            System.Diagnostics.Debug.WriteLine(e.ToString());
+        }
+
+        myText = GameObject.Find("Canvas2").GetComponent<Text>();
+
+        GameObject ethan = GameObject.Find("Ethan2");
+        c = (ThirdPersonCharacter)ethan.GetComponent(typeof(ThirdPersonCharacter));
+        anim = (Animator)ethan.GetComponent(typeof(Animator));
+    }
+
+    float x, y, z = 0;
+    Boolean hasUpdate = false;
+
+    private void recv(IAsyncResult res)
+    {
+        //System.Diagnostics.Debug.WriteLine("received something");
+
+        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, listenPort);
+        byte[] receive_byte_array = listener.EndReceive(res, ref RemoteIpEndPoint);
+        received_data = Encoding.UTF8.GetString(receive_byte_array);
+        //System.Diagnostics.Debug.WriteLine(Encoding.UTF8.GetString(receive_byte_array));
+
+        // parse string
+        string[] splits = received_data.Split(',');
+        x = float.Parse(splits[0]);
+        y = float.Parse(splits[1]);
+        z = float.Parse(splits[2]);
+
+        // scale
+        int scalingFactor = 10; // 1/10th scale
+        x = x / scalingFactor;
+        y = y / scalingFactor;
+        z = z / scalingFactor;
+
+        //System.Diagnostics.Debug.WriteLine("done translating, listening again...");
+
+        hasUpdate = true;
+        listener.BeginReceive(new AsyncCallback(recv), null);
+    }
+
+    private void Update()
+    {
+        if (hasUpdate)
+        {
+            float oldx, oldy, oldz;
+            //System.Diagnostics.Debug.WriteLine("Translate from " + oldx + " " + oldy + " " + oldz);
+            //System.Diagnostics.Debug.WriteLine("About to translate " + x + " " + y + " " + z);
+
+            //Process codes
+
+            oldx = this.gameObject.transform.position.x;
+            oldy = this.gameObject.transform.position.y;
+            oldz = this.gameObject.transform.position.z;
+
+            Vector3 start = new Vector3(oldx, oldy, oldz);
+            Vector3 end = new Vector3(x, y, z);
+            var distanceTo = start - end;
+            var smooth = 0.1f;
+
+            // manual control
+            transform.position = Vector3.Lerp(start, end, Time.deltaTime * smooth);
+
+            // game play control
+            //c.Move(end, false, false);
+
+            // translates by an offset
+            //transform.Translate(x, y, z, Space.World);
+
+            // herky jerky motion
+            //transform.position = end;
+
+            Vector3 oldVec = new Vector3(oldx, oldy, oldz);
+            Vector3 newVec = new Vector3(x, y, z);
+            Vector3 direction = newVec - oldVec;
+            //            Vector3 onlyY = new Vector3(0, direction.y, 0);
+
+            Quaternion oldrot = transform.rotation;
+            Quaternion newrot = Quaternion.LookRotation(direction, Vector3.up);
+
+            // jerky way
+            //             transform.rotation = newrot;
+            float turnrate = 30;
+            transform.rotation = Quaternion.RotateTowards(oldrot, newrot, turnrate * Time.deltaTime);
+
+            // display x,y,z on canvas
+            myText.text =
+                x + Environment.NewLine +
+                y + Environment.NewLine +
+                z;
+
+            hasUpdate = false;
+        }
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        System.Diagnostics.Debug.WriteLine("selected");
+        float oldx, oldy, oldz;
+        oldx = this.gameObject.transform.position.x;
+        oldy = this.gameObject.transform.position.y;
+        oldz = this.gameObject.transform.position.z;
+
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        //if (!this.GetComponent<Rigidbody>())
+        //{
+        //    var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+        //    rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        //}
+    }
+}
