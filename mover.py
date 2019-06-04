45505fcabfe6e9615a2874cf3b4dbf82f3b79b7a
Eric Riebling <er1k@andrew.cmu.edu>
2019-06-04 15:21:05 -0400

    finally

--- mover.py
+++ mover.py
@@ -2,27 +2,32 @@
 #
 # send x,y,z move coordinates like "1,2,1" to a UDP port (on hololens)
 
-import socket,time,random
+import socket,threading,SocketServer,time,random,os,sys
 
 # CMU_DEVICES
 hololens_ip="172.26.197.103"
 # ARENA
 #hololens_ip="192.168.1.239"
 
-FRAMERATE=10
+OBJECT=sys.argv[1]
 
+FRAMERATE=10
 framerate=1/FRAMERATE # sleep seconds 'frame rate'
-incr=0.2
+incr=0.1 # max incremental distance to move per frame
+
+TOPIC="/topic/render"
+# UDP send ports (deprecated)
 port1=9005
 port2=9006
+# listen
+HOST, PORT = "128.237.154.148", 9007
+
 x=0.0
 y=0.0
 z=0.0
 MESSAGE=""
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-
+# goofy random walk algorithm I made up :)
 def randmove():
     rando=random.random()
     if rando < 0.33:
@@ -37,28 +42,13 @@ Xaway = True
 Zaway = True
 
 while True:
-    MESSAGE=str(x)+','+str(y)+','+str(z)
-    print("Message: " + MESSAGE)
-    sock.sendto(MESSAGE, (hololens_ip, port2))
-    if (x > 60):
-        Xaway = False;
-    elif (x < 0):
-        Xaway = True;
+    MESSAGE=OBJECT+","+"{0:0.3f}".format(x)+','+"{0:0.3f}".format(y)+','+"{0:0.3f}".format(z)+",on"
+    print(MESSAGE)
 
-    if Xaway:
-        x+=randmove()
-    else:
-        x-=randmove()
+    os.system("mosquitto_pub -h oz.andrew.cmu.edu -t " + TOPIC + " -m " + MESSAGE);
 
-    if (z > 60):
-        Xaway = False;
-    elif (z < 0):
-        Xaway = True;
-
-    if Zaway:
-        z+=randmove()
-    else:
-        z-=randmove()
+    x+=randmove()
+    z+=randmove()
     
     time.sleep(framerate)
 
