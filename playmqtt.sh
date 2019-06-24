fe1174aa236247d89ba8f2eab3f4dbcc78332ba6
Eric Riebling <er1k@andrew.cmu.edu>
2019-06-24 14:23:24 -0400

    un-comment out publish

--- playmqtt.sh
+++ playmqtt.sh
@@ -1,5 +1,5 @@
 while read line; do
-    #mosquitto_pub -h oz.andrew.cmu.edu -t /topic/skeleton -m $line
+    mosquitto_pub -h oz.andrew.cmu.edu -t /topic/skeleton -m $line
     echo mosquitto_pub -h oz.andrew.cmu.edu -t /topic/skeleton -m $line
     sleep 1
 done
